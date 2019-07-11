772714f40f40677a5935ce84a430c6d36e9e90cc
tarisiddiq <ayit5902@gmail.com>
2019-07-11 12:43:27 -0700

    test

--- www/index.php
+++ www/index.php
@@ -1,66 +1,61 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      table{
-        width: 840px;
-        margin: auto;
-      }
-      h1{
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Tabel Biodata Mahasiswa</h1>
-    <center><a href="input.php">Input Data &Gt; </a></center>
-    <br/>
-    <table border="1">
-      <tr>
-        <th>No</th>
-        <th>NIM</th>
-        <th>Nama</th>
-        <th>Fakultas</th>
-        <th>Jurusan</th>
-        <th>IPK</th>
-        <th>Pilihan</th>
-      </tr>
-      <?php
-      // Connexion et sélection de la base
-      $conn = mysqli_connect('localhost','root','','crud_sederhana');
-      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
-      $query = "SELECT * FROM mahasiswa ORDER BY nim ASC";
-      $result = mysqli_query($link, $query);
-      //mengecek apakah ada error ketika menjalankan query
-      if(!$result){
-        die ("Query Error: ".mysqli_errno($link).
-           " - ".mysqli_error($link));
-      }
+<?php 
 
-      //buat perulangan untuk element tabel dari data mahasiswa
-      $no = 1; //variabel untuk membuat nomor urut
-      // hasil query akan disimpan dalam variabel $data dalam bentuk array
-      // kemudian dicetak dengan perulangan while
-      while($data = mysqli_fetch_assoc($result))
-      {
-        // mencetak / menampilkan data
-        echo "<tr>";
-        echo "<td>$no</td>"; //menampilkan no urut
-        echo "<td>$data[nim]</td>"; //menampilkan data nim
-        echo "<td>$data[nama]</td>"; //menampilkan data nama
-        echo "<td>$data[fakultas]</td>"; //menampilkan data fakultas
-        echo "<td>$data[jurusan]</td>"; //menampilkan data jurusan
-        echo "<td>$data[ipk]</td>"; //menampilkan data ipk
-        // membuat link untuk mengedit dan menghapus data
-        echo '<td>
-          <a href="edit.php?id='.$data['id'].'">Edit</a> /
-          <a href="hapus.php?id='.$data['id'].'"
-      		  onclick="return confirm(\'Anda yakin akan menghapus data?\')">Hapus</a>
-        </td>';
-        echo "</tr>";
-        $no++; // menambah nilai nomor urut
-      }
-      ?>
-    </table>
-  </body>
+session_start();
+if(empty($_SESSION['username'])){
+	header('location:form_login.php');
+}
+include"koneksi.php";
+$sqlu=mysqli_query($koneksi,"select * from  user u,  wisata w where u.id_wisata=w.id_wisata and u.username='$_SESSION[username]'") or die ('error');//memanggil data user berdasarkan session
+$datau=mysqli_fetch_array($sqlu);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Latihan Bootstrap</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-pluid"><!--container adalah untuk mengafitsemua div -->
+	<div style="background-color:#2196F3; color:#fff; height:100px; padding:15px;"><!--ini adalah header-->
+		<h3><b> Wisata</b></h3>
+
+			</div> 
+			  <div class="row" ><!--ini adalah row-->
+				<div class="col-sm-3"><!--ini adalah pembagian colom -->
+			<ul class="list-group"><!--ini adalah list group -->
+				<a href="index.php?mod=home" class="list-group-item">HOME</a>
+					<a href="index.php?mod=tampil_data_user" class="list-group-item">USER</<a>
+							<a href="index.php?mod=tampil_data_wisata" class="list-group-item">WISATA</a>
+								<a href="index.php?mod=laporan" class="list-group-item">LAPORAN</a>
+									<a href="logout.php" class="list-group-item">LOG OUT</a>
+									</ul>
+										</div>
+									<div class="col-sm-9">
+								<div style="border:0px black solid; padding:5px;"><!--ini adalah untuk membuat contern dengan border -->
+			<?php
+				if(empty($_GET['mod'])){
+					include"home.php";
+					}else{
+				$mod=$_GET['mod'];
+				include $mod.".php";
+			}
+			?>
+								</div>
+						</div>
+				</div>
+				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+				<div style="background-color:#2196F3; color:#ffffff; height:30px; padding:5px;"><!--ini adalah footer -->
+			<center><p style="font:11px verdana">
+				
+				 Email : <b>siddiqfisamalaka@gmail.com<br/>
+		</p></center>
+	</div>
+</div>
+</body>
+</html>
 </html>
